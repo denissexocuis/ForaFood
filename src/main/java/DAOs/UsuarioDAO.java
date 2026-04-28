97a4fce1232baea0454c2916356c1ef7cb4b05e8
denissexocuis <denissexocuis@gmail.com>
2026-04-28 12:11:56 -0600

    haciendo primer DAO y pruebas de insert

---
+++ src/main/java/DAOs/UsuarioDAO.java
@@ -0,0 +1,10 @@
+package DAOs;
+
+// el usuario se podrá registrar y asi jsdjsdf
+
+public class UsuarioDAO
+{
+    private static final String insertSQL =
+            "INSERT INTO users (nombre, passwd)"
+                    + "VALUES (?, ?)";
+}
