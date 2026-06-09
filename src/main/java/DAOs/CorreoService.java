d55958fee183f14a197d8e507cc4dc15c7329355
denissexocuis <denissexocuis@gmail.com>
2026-06-08 23:53:49 -0600

    fix: agregar archivos que faltaban en repo

---
+++ src/main/java/DAOs/CorreoService.java
@@ -0,0 +1,38 @@
+package DAOs;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+//* esto fue IAdo :), no sabía como hacerle :(
+
+public class CorreoService
+{
+
+    public static void mandarCodigo(String correoDestino, int codigo) throws MessagingException {
+
+        String correoOrigen = System.getenv("CORREO");
+        String password = System.getenv("CORREO_PASSWORD");
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(correoOrigen, password);
+            }
+        });
+
+        Message mensaje = new MimeMessage(session);
+        mensaje.setFrom(new InternetAddress(correoOrigen));
+        mensaje.setRecipients(Message.RecipientType.TO, InternetAddress.parse(correoDestino));
+        mensaje.setSubject("Código de verificación - ForaFood");
+        mensaje.setText("Tu código de verificación es: " + codigo);
+
+        Transport.send(mensaje);
+        System.out.println("Correo mandado a: " + correoDestino);
+    }
+}
