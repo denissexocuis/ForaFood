065af69a595c99ebee305085cdabd553f8f99421
schiliskiller <alangamer063@gmail.com>
2026-06-12 02:53:07 -0600

    fix: unificar puerto 465 ssl para local y nube

--- src/main/java/DAOs/CorreoService.java
+++ src/main/java/DAOs/CorreoService.java
@@ -16,11 +16,12 @@ public class CorreoService
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+       // props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         //props.put("mail.smtp.port", "587"); // local host
 
         // deployment
+        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.socketFactory.port", "465");
