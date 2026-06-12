3d4cb42a5b955babdb36061b030f5204e809f3e4
schiliskiller <alangamer063@gmail.com>
2026-06-12 03:39:39 -0600

    dejando como estaba al inicio

--- src/main/java/DAOs/CorreoService.java
+++ src/main/java/DAOs/CorreoService.java
@@ -15,40 +15,22 @@ public class CorreoService
         String password = System.getenv("CORREO_PASSWORD");
 
         Properties props = new Properties();
-        //props.put("mail.smtp.auth", "true");
-        //rops.put("mail.smtp.starttls.enable", "true");
-        //props.put("mail.smtp.host", "smtp.gmail.com");
-        //props.put("mail.smtp.port", "587"); // local host
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587"); // local host
 
         // deployment
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.socketFactory.fallback", "false");
-        props.put("mail.smtp.quitwait", "false");
+        // TODO
 
-        props.put("mail.smtp.user", correoOrigen);
-        props.put("mail.smtp.password", password);
-
-        Session session = Session.getDefaultInstance(props, new Authenticator() {
-            @Override
+        // localhost
+        Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(correoOrigen, password);
             }
         });
 
-        // localhost
-        //Session session = Session.getInstance(props, new Authenticator() {
-        //    protected PasswordAuthentication getPasswordAuthentication() {
-        //        return new PasswordAuthentication(correoOrigen, password);
-        //    }
-        //});
-
-        //session.setDebug(true);
+        session.setDebug(true);
 
         Message mensaje = new MimeMessage(session);
         mensaje.setFrom(new InternetAddress(correoOrigen));
